8a81e190d2d8b51e5d0e422bbba37e0dd55bdbc5
vvelikov00 <v.velikov00@abv.bg>
2021-05-13 16:48:57 +0300

    updated

--- myscript.js
+++ myscript.js
@@ -1,74 +1,67 @@
 
+/*jslint browser:true */
+/*global $, jQuery, alert*/
 function mobileNav() {
-  var x = document.getElementById("myLinks");
-  var y = document.getElementById("currentpage");
+  'use strict';
+  var x, y;
+  x = document.getElementById("myLinks");
+  y = document.getElementById("currentpage");
 
-  if (x.style.display == "block" ) {
-    x.style.display = "none";
+  if (x.style.display === "block") {
+      x.style.display = "none";
   } else {
 
-    x.style.display = "block";
+      x.style.display = "block";
 
   }
 
-  if (y.style.display == "none" ) {
-    y.style.display = "block";
+  if (y.style.display === "none") {
+      y.style.display = "block";
   } else {
 
-    y.style.display = "none";
+      y.style.display = "none";
 
   }
 }
 
-window.onresize = function(){mobnavbar(), route()};
+
 function mobnavbar() {
-   var x = window.outerWidth;
-   var y = window.outerHeight;
-  if ( x > 800) {
-    document.getElementById("myLinks").style.display = "block";
-  }  
- 
-  else if( x <= 800)
-  {
-   document.getElementById("myLinks").style.display = "none";
+  'use strict';
+  var x;
+  x = window.outerWidth;
+  if (x > 800) {
+      document.getElementById("myLinks").style.display = "block";
+  } else if (x <= 800) {
+      document.getElementById("myLinks").style.display = "none";
   }
 }
 
 
 function showonly(elem) {
+  'use strict';
   var id = $(elem).attr("id");
   $('.route').hide();
-  if(id == 'all'){
-    $('.route').show();
-  }
-  else{
-  $('[data-id=' + id + ']').show();
+  if (id === 'all') {
+      $('.route').show();
+  } else {
+      $('[data-id=' + id + ']').show();
   }
 }
 
-
-
-
-
-
-
-window.onscroll = function() {scrollFunction(), stickybar()};
-
-
-  var mybutton = document.getElementById("topBtn");
-
-
+var mybutton = document.getElementById("topBtn");
 
 function scrollFunction() {
+  'use strict';
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+      mybutton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+      mybutton.style.display = "none";
   }
 }
 
 
 function topFunction() {
+  'use strict';
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
@@ -77,11 +70,15 @@ function topFunction() {
 
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
+
 function stickybar() {
+  'use strict';
   if (window.pageYOffset > sticky) {
-    navbar.classList.add("sticky");
-  } else{
-    navbar.classList.remove("sticky");
+      navbar.classList.add("sticky");
+  } else {
+      navbar.classList.remove("sticky");
   }
 }
 
+window.onresize = function () {'use strict'; mobnavbar(); };
+window.onscroll = function () {'use strict'; scrollFunction(); stickybar(); };
